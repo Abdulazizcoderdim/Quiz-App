0bec4e8cf60ead9d7aba097bc7d29025061b385f
AbdulazizRus <coderdim@gmail.com>
2024-08-01 21:15:41 +0500

    changes

---
+++ src/components/MenuLinks.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const MenuLinks = () => {
+  return <div>MenuLinks</div>
+}
+
+export default MenuLinks
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,18 @@
+import { MenuLinks } from "../components"
+
 const Home = () => {
-  return <div>Home</div>
+  return <section className="home-container container">
+     <div className="home-content">
+      <h1 className="home-title">
+        <span>Welcome to the</span>
+        <span>Frontend Quiz!</span>
+      </h1>
+      <p>Pick a subject to get started.</p>
+     </div>
+     <div className="home-nav-list">
+      <MenuLinks/>
+     </div>
+  </section>
 }
 
 export default Home
