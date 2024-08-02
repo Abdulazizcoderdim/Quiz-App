952fd70b4e01d67f0423386df66352def456de7b
AbdulazizRus <coderdim@gmail.com>
2024-08-02 10:34:53 +0500

    asas

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,11 @@
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <footer>
+      <Link to={'https://github.com/Abdulazizcoderdim'}>GitHub</Link>
+    </footer>
+  )
 }
 
 export default Footer
